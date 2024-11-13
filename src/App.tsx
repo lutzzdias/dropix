163ec90840add51d98380a872668982afa35e94b
lutzzdias <thiagolutzdias@hotmail.com>
2024-11-12 23:47:15 -0400

    feat: use components in app
    gets everything together and working

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+import { ProductList } from "./components/ProductList";
+import { VideoArea } from "./components/VideoArea";
+
 export function App() {
-  return <>Hello World!</>;
+  const [videoInfo, setVideoInfo] = useState<{
+    title: string;
+    subtitle: string;
+    url: string;
+  } | null>(null);
+  const [products, setProducts] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api-candidate.ogruposix.com/checkout/95BD9233-8FDC-48AD-B4C5-E5BAF7578C15",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "user-token": "2A50C22E-7954-4C73-9CF9-F6D298C047A7",
+            },
+          },
+        );
+        const data = (await response.json()).object[0];
+        setVideoInfo({
+          title: data.video_headline,
+          subtitle: data.video_sub_headline,
+          url: data.video_url,
+        });
+        setProducts(data.products);
+      } catch (error) {
+        alert("Failed to fetch data. Please try again later.");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex max-w-5xl flex-col gap-6">
+      {videoInfo && <VideoArea info={videoInfo} />}
+      {products.length > 0 && <ProductList products={products} />}
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { App } from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="flex flex-1 flex-col items-center p-4">
+      <App />
+    </div>
   </StrictMode>,
 );
