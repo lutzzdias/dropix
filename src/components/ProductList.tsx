977af9dea223c14c26c0f9b9f16019b473fe2e3f
lutzzdias <thiagolutzdias@hotmail.com>
2024-11-12 23:46:40 -0400

    feat: product list component

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,19 @@
+import { ProductCard } from "./ProductCard";
+
+export function ProductList(props: any) {
+  const { products } = props;
+
+  return (
+    <div className="flex max-w-2xl flex-col gap-4 lg:max-w-full">
+      <h2 className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+        Products
+      </h2>
+
+      <div className="grid grid-cols-1 justify-items-center gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        {products.map((product: any) => (
+          <ProductCard key={product.product_id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
